b6547708fa84e5e964843609dbc5f85746691d03
w865629524 <424113477@qq.com>
2019-05-27 07:17:06 +0800

    update

--- src/test/java/com/scu/hub/entity/DepositoryTest.java
+++ src/test/java/com/scu/hub/entity/DepositoryTest.java
@@ -1,56 +1,73 @@
 package com.scu.hub.entity;
 
+import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class DepositoryTest {
 
+    Depository depository;
+    @Before
+    private void before() {
+        depository = new Depository(111, "fdsaga", "//", true, 11, "tag");
+    }
+
     @Test
     public void getDepositoryId() {
+        System.out.println(depository.getDepositoryId());
     }
 
     @Test
     public void setDepositoryId() {
+        depository.setDepositoryId(1243);
     }
 
     @Test
     public void getDepositoryName() {
+        System.out.println(depository.getDepositoryName());
     }
 
     @Test
     public void setDepositoryName() {
+        depository.setDepositoryName("1243");
     }
 
     @Test
     public void getDepositoryPath() {
+        System.out.println(depository.getDepositoryPath());
     }
 
     @Test
     public void setDepositoryPath() {
+        depository.setDepositoryPath("1");
     }
 
     @Test
     public void getDepositoryVisiable() {
+        System.out.println(depository.getDepositoryVisiable());
     }
 
     @Test
     public void setDepositoryVisiable() {
+        depository.setDepositoryVisiable(false);
     }
 
     @Test
     public void getDepositoryClassificationId() {
+        System.out.println(depository.getDepositoryClassificationId());
     }
 
     @Test
     public void setDepositoryClassificationId() {
+        depository.setDepositoryClassificationId(1);
     }
 
     @Test
     public void getTags() {
+        System.out.println(depository.getTags());
     }
 
     @Test
     public void setTags() {
+        depository.setTags("4132");
     }
 }
--- src/test/java/com/scu/hub/entity/UserTest.java
+++ src/test/java/com/scu/hub/entity/UserTest.java
@@ -1,64 +1,85 @@
 package com.scu.hub.entity;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.sql.Date;
 
 public class UserTest {
 
+    User user;
+    @Before
+    private void before(){
+        user = new User("user123", "namename", "123456", true, 11, new Date(2000, 01, 01), "dedde");
+    }
+
     @Test
     public void getUserId() {
+        System.out.println(user.getUserId());
     }
 
     @Test
     public void setUserId() {
+        user.setUserId("11");
     }
 
     @Test
     public void getUsername() {
+        System.out.println(user.getUsername());
     }
 
     @Test
     public void setUsername() {
+        user.setUsername("11");
     }
 
     @Test
     public void getPassword() {
+        System.out.println(user.getPassword());
     }
 
     @Test
     public void setPassword() {
+        user.setPassword("11");
     }
 
     @Test
     public void getGender() {
+        System.out.println(user.getGender());
     }
 
     @Test
     public void setGender() {
+        user.setGender(false);
     }
 
     @Test
     public void getAge() {
+        System.out.println(user.getAge());
     }
 
     @Test
     public void setAge() {
+        user.setAge(11);
     }
 
     @Test
     public void getBirth() {
+        System.out.println(user.getBirth());
     }
 
     @Test
     public void setBirth() {
+        user.setBirth(new Date(2000, 01, 01));
     }
 
     @Test
     public void getDescription() {
+        System.out.println(user.getDescription());
     }
 
     @Test
     public void setDescription() {
+        user.setDescription("14512334");
     }
 }
