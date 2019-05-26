580b22615990650779b88a2ea0b401506e94708b
w865629524 <424113477@qq.com>
2019-05-27 02:55:13 +0800

    ..

--- ScuNoteHub/src/main/java/com/scu/hub/controller/enumClass/DepositoryVisible.java
+++
@@ -1,6 +0,0 @@
-package com.scu.hub.controller.enumClass;
-
-public class DepositoryVisible {
-    public static final Integer UNVISIBLE = 0;//不可见
-    public static final Integer VISIBLE = 1;//可见
-}
--- ScuNoteHub/src/main/java/com/scu/hub/controller/enumClass/ResponseStatus.java
+++
@@ -1,6 +0,0 @@
-package com.scu.hub.controller.enumClass;
-
-public class ResponseStatus {
-    public static final Integer FAIL = 0;//失败
-    public static final Integer SUCCESS = 1;//成功
-}
